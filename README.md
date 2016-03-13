a29530eebb5d24ab5425e4303402eedbdc0351dd
ComeOnGetMe <yzp451312410@gmail.com>
2016-03-13 16:11:39 +0800

    script optimized (the first edition is sooooooo ugly...)

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 
 Google tesseract font auto-trainning script
 Following the guide provided by [tim.oconnell](http://www.resolveradiologic.com/blog/2013/01/15/training-tesseract/).
-Edited from [https://github.com/this-is-ari](https://github.com/this-is-ari)'s script.
--- tesseract_train.py
+++ tesseract_train.py
@@ -1,26 +1,24 @@
 # Tesseract 3.02 Font Trainer
+# V0.01 - 3/04/2013
 
+# Complete the documentation 
 import time
 import os
 from sys import argv
-
-__author__ = 'zpyan'
-
 fontname = '9p'
 language = 'mon'
 directory = "./"
 
-print 'Tesseract Font Builder - assumes training TIFFs and boxfiles already created'
+print 'Tesseract Font Builder'
+print 'Assumes font_properties, training TIFFs and boxfiles already created'
 print 'Note: Only up to 32 .tiff files are supported for training purposes'
 count = 0
 for files in os.listdir(directory):
     
     if files.endswith(".tif"):
         #Train the boxfiles
-        rename = 'mv '+files+' '+language+'.'+fontname+'.exp'+str(count)+'.tif'
-        os.system(rename)
-        command='tesseract eng.'+fontname+'.exp'+str(count)+'.tif eng.'+fontname+'.exp'+str(count)+' nobatch box.train.stderr'
-        print command   
+        command = 'tesseract {0}.{1}.exp{2}.tif {0}.{1}exp{2} nobatch box.train.stderr'.format(language, fontname, count)
+        print '-----------------------\n' + command + '-----------------------\n'
         os.system(command)
         count = count + 1
 
@@ -28,37 +26,33 @@ trfilelist = ''
 boxfilelist = ''
 font_properties = ''
 
-for files in os.listdir(directory):
-    if files.endswith(".tr"):
-        trfilelist = trfilelist + ' ' + files    
-        font_properties = fontname+' 0 0 0 0 0'    
-    if files.endswith(".box"):
-        boxfilelist = boxfilelist + ' ' + files
+trfilelist = ' '.join([files for files in os.listdir(directory) if files.endswith('.tr')])
+boxfilelist = ' '.join([files for files in os.listdir(directory) if files.endswith('.box')])
 
 #Build the Unicharset File
 command2 = 'unicharset_extractor ' + boxfilelist
-print command2
+print '-----------------------\n' + command2 + '-----------------------\n'
 os.system(command2)
 
 #Build the font properties file
-fontpropertiesfile = open('font_properties', 'a+') # saving into a file        
-fontpropertiesfile.write(font_properties)
-print 'Wrote font_properties file'
-fontpropertiesfile.close()
+# fontpropertiesfile = open('font_properties', 'a+') # saving into a file        
+# fontpropertiesfile.write(font_properties)
+# print 'Wrote font_properties file'
+# fontpropertiesfile.close()
 
-#Clustering
-command3 = 'shapeclustering -F font_properties -U unicharset' + trfilelist
-print command3
+# #Clustering
+command3 = 'shapeclustering -F font_properties -U unicharset ' + trfilelist
+print '-----------------------\n' + command3 + '-----------------------\n'
 os.system(command3)
 
 #MFTraining
-mftraining = 'mftraining -F font_properties -U unicharset -O '+language+'.charset '+trfilelist
-print mftraining
+mftraining = 'mftraining -F font_properties -U unicharset -O '+ language +'.charset '+trfilelist
+print '-----------------------\n' + mftraining + '-----------------------\n'
 os.system(mftraining)
 
 #CNTraining
 command4 = 'cntraining ' + trfilelist
-print command4
+print '-----------------------\n' + command4 + '-----------------------\n'
 os.system(command4)
 
 #Rename necessary files
@@ -72,5 +66,4 @@ command5 = 'combine_tessdata '+language+'.'
 os.system(command5)
 
 #Copy it over
-command6 = 'cp -f '+language+'.traineddata /usr/local/share/tessdata'
-os.system(command6)
+# Don't forget to copy language.traineddata to your tessdata repo!
